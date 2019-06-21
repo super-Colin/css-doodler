44989be48e28727fc1e398c484c81d809d45cc4d
bigwheelsmith <csmith@gobigwheel.com>
2019-06-20 21:56:39 -0400

    cell border implemented

--- doodler.js
+++ doodler.js
@@ -1,4 +1,3 @@
-
 function refresh_doodle() {
     let doodle = document.getElementById('doodle');
 
@@ -15,57 +14,89 @@ function refresh_doodle() {
     let cell_l = document.getElementById('cell_l').value;
 
     let cell_center = document.getElementById('cell_center').checked;
-    if(cell_center === true){
+    if (cell_center === true) {
         cell_center = "@place-cell: center;"
     } else {
         cell_center = "";
     }
 
-    let cell_radius_check = document.getElementById('cell_radius_check').checked;
 
-    let cell_border_radius = "";
-    if(cell_radius_check === true){
-        let cell_radius = document.getElementById('cell_radius').value;
-        cell_border_radius = 'border-radius:'+ cell_radius +'%;';
-    } else {
-        cell_border_radius = "";
-    }
-
-    
+    // doodle border stuff
 
     let doodle_radius_check = document.getElementById('doodle_radius_check').checked;
-
     let doodle_border_radius = "";
-
-    if(doodle_radius_check === true){
+    if (doodle_radius_check === true) {
         let doodle_radius = document.getElementById('doodle_radius').value;
-        doodle_border_radius = 'border-radius:'+ doodle_radius +'%;';
+        doodle_border_radius = 'border-radius:' + doodle_radius + '%;';
     }
-    //  else {
-    //     doodle_border_radius = "";
-    // }
+
 
     let doodle_border_style_check = document.getElementById('doodle_border_style_check').checked;
-
     let doodle_border_style = "";
-    if(doodle_border_style_check === true){
+    if (doodle_border_style_check === true) {
         let e = document.getElementById("doodle_border_style");
         doodle_border_style = e.options[e.selectedIndex].text;
-        doodle_border_style = 'border-style:'+ doodle_border_style +';';
+        doodle_border_style = 'border-style:' + doodle_border_style + ';';
         console.log('worked');
-    } 
+    }
 
-    
+
     let doodle_border_width_check = document.getElementById('doodle_border_width_check').checked;
-
     let doodle_border_width = "";
-    if(doodle_border_width_check === true){
+    if (doodle_border_width_check === true) {
         let width = document.getElementById('doodle_border_width').value;
-        doodle_border_width = 'border-width: '+ width +'px;';
-    } 
-    // else {
-    //     doodle_border_width = "";
+        doodle_border_width = 'border-width: ' + width + 'px;';
+    }
+
+
+
+
+    // end doodle border stuff
+
+    // ============================
+    // cell border stuff
+
+    let cell_radius_check = document.getElementById('cell_radius_check').checked;
+    let cell_border_radius = "";
+    if (cell_radius_check === true) {
+        let cell_radius = document.getElementById('cell_radius').value;
+        cell_border_radius = 'border-radius:' + cell_radius + '%;';
+    }
+
+
+    let cell_border_style_check = document.getElementById('cell_border_style_check').checked;
+    let cell_border_style = "";
+    if (cell_border_style_check === true) {
+        let e = document.getElementById("cell_border_style");
+        cell_border_style = e.options[e.selectedIndex].text;
+        cell_border_style = 'border-style:' + cell_border_style + ';';
+        console.log('worked');
+    }
+
+
+    let cell_border_width_check = document.getElementById('cell_border_width_check').checked;
+    let cell_border_width = "";
+    if (cell_border_width_check === true) {
+        let width = document.getElementById('cell_border_width').value;
+        cell_border_width = 'border-width: ' + width + 'px;';
+
+        // This is where the @index is checked for
+        let cell_border_index_check = document.getElementById('cell_border_index_check').checked;
+        if (cell_border_index_check === true) {
+            cell_border_width = 'border-width: calc(@i() * ' + width + 'px);'
+        }
+
+    }
+
+
+    // let cell_border_index_check = document.getElementById('cell_border_index_check').checked;
+    // if(cell_border_index_check === true){
+    //     cell_border_width = 'calc(@i() * '+ width +');'
     // }
+
+    // end cell border stuff
+
+    // ==================
 
 
 
@@ -75,19 +106,20 @@ function refresh_doodle() {
 
     let doodle_input = `
         :doodle{
-            @grid: `+ gridx +`x`+ gridy +`;
+            @grid: ` + gridx + `x` + gridy + `;
             @size: 50vmax;
-            background-color: hsl(`+ doodle_h +`, `+ doodle_s +`%, `+ doodle_l +`%);
-            `+ doodle_border_radius +`
-            `+ doodle_border_style +`
-            `+ doodle_border_width +`
+            background-color: hsl(` + doodle_h + `, ` + doodle_s + `%, ` + doodle_l + `%);
+            ` + doodle_border_radius + `
+            ` + doodle_border_style + `
+            ` + doodle_border_width + `
             border-color: red;
             overflow:hidden;
         }
         --hue: calc( 100 + 1 * @row() * @col());
-        `+ cell_center +`
-        `+ cell_border_radius +`
-        `+ doodle_border_width +`
+        ` + cell_center + `
+        ` + cell_border_radius + `
+        ` + cell_border_style + `
+        ` + cell_border_width + `
 
 
         background-color: hsla(var(--hue), 50%, 70%, @rand(0, 90%));
